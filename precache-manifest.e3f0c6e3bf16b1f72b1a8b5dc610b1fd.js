2e8b37538eaa26bd3031c15d9b1ac79fe55d4e62
Shaghayegh shakerzadeh <s.shakerzadeh@asax.local>
2019-10-31 14:54:19 +0330

    Updates

---
+++ precache-manifest.e3f0c6e3bf16b1f72b1a8b5dc610b1fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbb0f23024a1b1965dabddaa8e0ccd8",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "121c3cb7ac58717e7e97",
+    "url": "/react-test/static/css/main.ed165793.chunk.css"
+  },
+  {
+    "revision": "a47dbd2adf81c47bc48e",
+    "url": "/react-test/static/js/2.e5c4c9e3.chunk.js"
+  },
+  {
+    "revision": "121c3cb7ac58717e7e97",
+    "url": "/react-test/static/js/main.b92a772a.chunk.js"
+  },
+  {
+    "revision": "c251d0b15df01eacda8c",
+    "url": "/react-test/static/js/runtime-main.0591d0d9.js"
+  }
+]);
